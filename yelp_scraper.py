1a4ad318a4825202f07957206cce74f0e9537e9e
IshoWhoCodes <coderisho@localhost.localdomain>
2020-09-20 23:31:05 +0100

    updating yelp_scraper

--- yelp_scraper.py
+++ yelp_scraper.py
@@ -1,18 +1,43 @@
 from selenium import webdriver
 import time
 
+"""
+search for terms and scrape data for each term, by scraping and crawling...
+"""
+
+
 class YelpBot(object):
     def __init__(self):
         self.url = "https://www.yelp.com/"
         self.driver = webdriver.Firefox()
         self.driver.get(self.url)
-    
+        
     def close_session(self):
         print("[+] Clossing Session...")
         time.sleep(4)
         self.driver.close()
 
+    def submit_button(self):
+        btn = self.driver.find_element_by_xpath("//*[@id='header-search-submit']")
+        btn.click()
+
+    def input_fields(self, service="takeout", location="San Francisco, CA"):
+        service_pl = self.driver.find_element_by_xpath("//*[@id='find_desc']")
+        location_pl = self.driver.find_element_by_xpath("//*[@id='dropperText_Mast']")
+        
+        service_pl.clear()
+        location_pl.clear()
+        
+        service_pl.send_keys(service)
+        location_pl.send_keys(location)
+
+    def extract_posts(self):
+        posts = self.driver.find_element_by_xpath("/html/body/div[1]/div[4]/div/div[1]/div[1]/div[2]/div[2]/ul")
+        print(posts.text)
 
 if __name__ == "__main__":
     st_yelp = YelpBot()
+    st_yelp.input_fields()
+    st_yelp.submit_button()
+    st_yelp.extract_posts()
     st_yelp.close_session()
